bcf3a01af066e0ef64081f8cdda631252135229a
Rebecca <rvkupper@gmail.com>
2021-02-24 15:39:48 +0100

    Started on CellLineRMAExpression, added function readRMAExpression

---
+++ CellLineRMAExpression.py
@@ -0,0 +1,51 @@
+"""Some text on this script
+"""
+
+class CellLineRMAExpression:
+    """class for analysis of cancer data.
+    """
+    # class variables
+    
+    
+    # instance variables
+    def __init__(self, type: str):
+        self.type = type
+        
+    def readRMAExpression(self, cellLine: str) -> list:
+        """read the RMA expression of a single cell line cellLine
+        
+        parameters:
+        cellLine: cell line from which the RMA expression is to be read 
+        
+        returns:
+        genes: list of all 244 genes of cellLine.
+        """
+        # read data
+        inf = open('data/GDSC_RNA_expression.csv')
+        lines = inf.readlines()
+        inf.close()
+        
+        nrGenes = len(lines[0])
+        
+        # catch non-existant cell lines 
+        # assert cellLine in lines.split(','), 'inserted cell line not found'
+        genes = None
+        cellLineIndex = 0
+        
+        for i, line in enumerate(lines[1:]):
+            lineList = line.split(',')
+            if lineList[0] == cellLine:
+                cellLineIndex = i 
+                genes = lineList[1:]
+                break
+        
+        # Convert strings to floats                
+        for j in range(len(genes)):
+            genes[j] = float(genes[j])
+        
+        return genes
+        
+        
+someCellLine = CellLineRMAExpression('something')
+genen = someCellLine.readRMAExpression('HT-29')
+print(len(genen))
